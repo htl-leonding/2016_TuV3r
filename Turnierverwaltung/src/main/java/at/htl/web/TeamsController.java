0323f71992de1c9911835ba3f6b7e456ee4ec0c0
LaurenzG <lllaugi@gmail.com>
2016-03-30 11:53:45 +0200

    completely reworking the teams page, adding functionality

--- Turnierverwaltung/src/main/java/at/htl/web/TeamsController.java
+++ Turnierverwaltung/src/main/java/at/htl/web/TeamsController.java
@@ -26,6 +26,7 @@ public class TeamsController implements Serializable {
     @Inject
     MatchFacade matchFacade;
 
+
     public List<Team> getTeams(){
         return teamFacade.findTeamsFromLatestTournament();
     }
@@ -33,9 +34,57 @@ public class TeamsController implements Serializable {
         return tournamentFacade.getTeamsOrderedByRank(tournamentFacade
                         .findLatestTournament());
     }
-    public Match getFinal(){
+    public Team getWinner(){
+        return getFinalMatch().getTeam1();
+    }
+    public Team getRunnerUp(){
+        return getFinalMatch().getTeam2();
+    }
+    private Match getFinal(){
         List<Match> matches= matchFacade.findMatchesByTeam(getTeamsByRank().get(0));
         return matches.get(matches.size());
+    }
+    public List<Team> getQuarterFinalists(){
+        return getTeamsByRank().subList(0,7);
+    }
+    public List<Team> getSemiFinalists(){
+        return getTeamsByRank().subList(0,3);
+    }
+    public List<Match> getQuarterFinalMatches(){
+        return matchFacade
+                .findAll()
+                .subList(matchFacade.findAll().size()-7
+                        ,matchFacade.findAll().size()-3);
+    }
+    public List<Match> getSemiFinalMatches(){
+        return matchFacade
+                .findAll()
+                .subList(matchFacade.findAll().size()-3
+                        ,matchFacade.findAll().size()-1);
+    }
+    public Match getFinalMatch(){
+        return matchFacade
+                .findAll()
+                .get(matchFacade.findAll().size()-1);
+    }
+
+    public List<Match> getRecentMatches(Team team){
+        return matchFacade.findMatchesByTeam(team);
+    }
+
+    public Team getOpponent(Team team, List<Match> matches){
+        for (Match match : matches) {
+            if(match.getTeam1().getName().equals(team.getName())){
+                return match.getTeam2();
+            }
+            if(match.getTeam2().getName().equals(team.getName())){
+                return match.getTeam1();
+            }
+        }
+        return null;
+    }
+    public int getScore(Team team, Match match){
+        return 0;
     }
 
 
